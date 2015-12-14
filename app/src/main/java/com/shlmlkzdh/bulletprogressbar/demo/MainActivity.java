5c22a3215d5ecbc3b8a9cce4261eebaa72037354
Soheil Malekzadeh <shl.mlkzdh@gmail.com>
2015-12-14 17:30:03 -0500

    Modify main activity

--- app/src/main/java/com/shlmlkzdh/bulletprogressbar/demo/MainActivity.java
+++ app/src/main/java/com/shlmlkzdh/bulletprogressbar/demo/MainActivity.java
@@ -3,8 +3,6 @@ package com.shlmlkzdh.bulletprogressbar.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.shlmlkzdh.bulletprogressbar.BulletProgressBar;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,21 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BulletProgressBar progressBar0 = (BulletProgressBar) findViewById(R.id.bpb0);
-        progressBar0.setProgress(0);
-
-        BulletProgressBar progressBar1 = (BulletProgressBar) findViewById(R.id.bpb1);
-        progressBar1.setProgress(1);
-
-        BulletProgressBar progressBar2 = (BulletProgressBar) findViewById(R.id.bpb2);
-        progressBar2.setProgress(2);
-
-        BulletProgressBar progressBar3 = (BulletProgressBar) findViewById(R.id.bpb3);
-        progressBar3.setProgress(3);
-
-        BulletProgressBar progressBar5 = (BulletProgressBar) findViewById(R.id.bpb5);
-        progressBar5.setProgress(5);
 
     }
 
